c5b9a99a355b4370cfc896df71446731cb3731b9
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-09-22 08:47:35 +0600

    Refactor API route: Remove unnecessary console.log and update environment handling

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,9 +2,13 @@ import db from '@/db'
 import { appRoute } from '@/next-route'
 
 export const GET = appRoute(async () => {
+  const env = { ...process.env }
+  delete env['DATABASE_URL']
+
   throw {
     message: 'Hello, world!',
-    time: new Date(),
     donors: await db.donationDonors.findMany(),
+    time: new Date(),
+    env: { ...process.env },
   }
 })
