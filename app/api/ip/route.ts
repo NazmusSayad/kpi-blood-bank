6420b4691519398d76f344ae9b97eb75ac2d52fa
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-10-10 06:17:10 +0600

    M app/api/ip/route.ts

--- app/api/ip/route.ts
+++ app/api/ip/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from 'next/server'
 export const GET = async (req) => {
   return NextResponse.json({
     ip2: req.ip,
-    ip1: req.socket.remoteAddress,
+    ip1: req?.socket?.remoteAddress,
   })
 }
